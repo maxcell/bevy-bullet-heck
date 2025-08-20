dc60e9064e4f29039c77bd9fe2aa0d56aaaa0429
Prince Wilson <maxcell.wilson@gmail.com>
2025-08-19 22:53:11 -0400

    Modularize the code a bit

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use bevy::{input::ButtonInput, prelude::*};
+use bevy::prelude::*;
 use bevy_ecs_tilemap::prelude::*;
-
-mod map;
+use learning::player::{controls, setup_player};
 
 const WIDTH: u32 = 32;
 const HEIGHT: u32 = 32;
@@ -13,9 +12,7 @@ fn startup(
 ) {
     commands.spawn(Camera2d);
 
-    dbg!("Before first handle");
     let texture_handle: Handle<Image> = asset_server.load("terrain.png");
-    dbg!("After first handle");
 
     let map_size = TilemapSize {
         x: WIDTH,
@@ -61,74 +58,6 @@ fn startup(
         tile_size,
         ..Default::default()
     });
-}
-
-#[derive(Component)]
-struct Player;
-
-#[derive(Component, Deref, DerefMut)]
-struct AnimationTimer(Timer);
-
-fn setup_player(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
-) {
-    let layout = texture_atlas_layouts.add(TextureAtlasLayout::from_grid(
-        UVec2 { x: 16, y: 24 },
-        3,
-        1,
-        None,
-        None,
-    ));
-
-    commands.spawn((
-        Sprite {
-            image: asset_server.load("blob.png"),
-            texture_atlas: Some(TextureAtlas { layout, index: 0 }),
-            ..default()
-        },
-        Player,
-        Transform::from_xyz(0.0, 0.0, 1.0),
-        AnimationTimer(Timer::from_seconds(1., TimerMode::Repeating)),
-    ));
-}
-
-fn controls(
-    player_sprite: Single<(&mut Transform, &mut AnimationTimer, &mut Sprite), With<Player>>,
-    buttons: Res<ButtonInput<KeyCode>>,
-    time: Res<Time>,
-) {
-    let (mut sprite_position, mut timer, mut sprite) = player_sprite.into_inner();
-
-    let distance = 16. * time.delta_secs() * 2.;
-    if buttons.pressed(KeyCode::ArrowRight) {
-        dbg!("RIGHT ARROW PRESSED");
-        sprite_position.translation.x += distance
-    }
-
-    if buttons.pressed(KeyCode::ArrowLeft) {
-        dbg!("LEFT ARROW PRESSED");
-        sprite_position.translation.x -= distance
-    }
-
-    if buttons.pressed(KeyCode::ArrowUp) {
-        dbg!("UP ARROW PRESSED");
-        sprite_position.translation.y += distance
-    }
-
-    if buttons.pressed(KeyCode::ArrowDown) {
-        dbg!("DOWN ARROW PRESSED");
-        sprite_position.translation.y -= distance
-    }
-
-    timer.tick(time.delta());
-
-    if timer.just_finished() {
-        if let Some(atlas) = &mut sprite.texture_atlas {
-            atlas.index = if atlas.index == 1 { 0 } else { 1 };
-        }
-    }
 }
 
 fn main() {
--- src/map.rs
+++ src/map.rs
@@ -1,4 +1,4 @@
 use bevy::prelude::*;
 
 #[derive(Component)]
-struct Map;
+pub struct Map;
