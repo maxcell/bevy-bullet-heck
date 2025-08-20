d4fd7b2c96b7c86a64ea362f9c6f831f6b6c7881
Prince Wilson <maxcell.wilson@gmail.com>
2025-08-19 22:13:24 -0400

    Updates to include animating sprite

--- src/main.rs
+++ src/main.rs
@@ -66,13 +66,16 @@ fn startup(
 #[derive(Component)]
 struct Player;
 
+#[derive(Component, Deref, DerefMut)]
+struct AnimationTimer(Timer);
+
 fn setup_player(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
 ) {
     let layout = texture_atlas_layouts.add(TextureAtlasLayout::from_grid(
-        UVec2 { x: 48, y: 24 },
+        UVec2 { x: 16, y: 24 },
         3,
         1,
         None,
@@ -87,14 +90,17 @@ fn setup_player(
         },
         Player,
         Transform::from_xyz(0.0, 0.0, 1.0),
+        AnimationTimer(Timer::from_seconds(1., TimerMode::Repeating)),
     ));
 }
 
 fn controls(
-    mut sprite_position: Single<&mut Transform, With<Player>>,
+    player_sprite: Single<(&mut Transform, &mut AnimationTimer, &mut Sprite), With<Player>>,
     buttons: Res<ButtonInput<KeyCode>>,
     time: Res<Time>,
 ) {
+    let (mut sprite_position, mut timer, mut sprite) = player_sprite.into_inner();
+
     let distance = 16. * time.delta_secs() * 2.;
     if buttons.pressed(KeyCode::ArrowRight) {
         dbg!("RIGHT ARROW PRESSED");
@@ -114,6 +120,14 @@ fn controls(
     if buttons.pressed(KeyCode::ArrowDown) {
         dbg!("DOWN ARROW PRESSED");
         sprite_position.translation.y -= distance
+    }
+
+    timer.tick(time.delta());
+
+    if timer.just_finished() {
+        if let Some(atlas) = &mut sprite.texture_atlas {
+            atlas.index = if atlas.index == 1 { 0 } else { 1 };
+        }
     }
 }
 
